f438c0ae1f2d99e58e223d94cd99dd0b9c55da09
Viorel-Gabriel MOCANU (94548) <viorel.mocanu@stud.acs.upb.ro>
2021-07-04 23:49:14 +0300

    Update: Add files for app-test-1
    Signed-off-by: Viorel-Gabriel MOCANU (94548) <viorel.mocanu@stud.acs.upb.ro>

---
+++ apps/app-test-1/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#ifdef __Unikraft__
+#include <uk/config.h>
+#endif /* __Unikraft__ */
+
+int function_3(int a)
+{
+	int i;
+	int ret = 0;
+	for(i = 0; i < 50000; i++) {
+		ret = a;
+		printf("aux = %d\n",ret);
+	}	
+	return ret;
+}
+
+
+int function_4(int a)
+{
+	int i = 0;
+	int ret = 0;	
+	for(i = 0; i < 50000; i++) {
+		ret = a;
+		printf("aux = %d\n",ret);
+	}
+	return ret;
+}
+
+
+int function_5(int a,int b)
+{
+	int i;
+	int ret = 0;	
+	for(i = 0; i < 50000; i++) {
+		ret = a + b;
+		printf("aux = %d\n",ret);
+	}
+
+	return ret;
+}
+
+int function_2(int a,int b)
+{
+	int ret = 0;
+	ret = function_4(b);
+	ret = function_5(a,b);
+	return ret;
+}
+
+int function_1(int a,int b)
+{
+
+	int ret = 0;	
+	ret = function_3(a);
+	return ret;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("Start\n");
+	int aux = 0;
+
+	aux = function_1(aux, 1);
+	aux = function_2(aux, 2);
+	printf("End\n");
+}
